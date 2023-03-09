fe0fcfc1ce8669333fc38fcb3ba8f5d2365bfa8f
Крот <@DESKTOP-8HJGMHB>
2023-03-09 19:03:29 +0300

    Добавьте файлы проекта.

---
+++ chtonibydcrytoe/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace chtonibydcrytoe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int value;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_click(object sender, RoutedEventArgs e)
+        {
+            value++;
+            text_block.Text = value.ToString();
+        }
+    }
+}
